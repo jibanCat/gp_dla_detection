24a3ba05c41d6f6beb8e733e03ca79ec0c7b09c3
jibancat <mho026@ucr.edu>
2020-04-27 16:58:06 -0700

    min_pix = 400; norm_min = 1216 - 20; max noise = 4^2

--- set_parameters.m
+++ set_parameters.m
@@ -50,10 +50,10 @@ loading_max_lambda = 5000;                  % This maximum is set so we include 
 z_qso_cut      = 2.15;         % filter out QSOs with z less than this threshold
 z_qso_training_max_cut = 5; % roughly 95% of training data occurs before this redshift; assuming for normalization purposes (move to set_parameters when pleased)
 z_qso_training_min_cut = 1.5; % Ignore these quasars when training
-min_num_pixels = 1000;                         % minimum number of non-masked pixels
+min_num_pixels = 400;                         % minimum number of non-masked pixels
 
 % normalization parameters
-normalization_min_lambda = lya_wavelength;              % range of rest wavelengths to use   Å
+normalization_min_lambda = lya_wavelength - 20;              % range of rest wavelengths to use   Å
 normalization_max_lambda = 1300;              %   for flux normalization
 
 % null model parameters
@@ -61,7 +61,7 @@ min_lambda         = lya_wavelength;                 % range of rest wavelengths
 max_lambda         = 2840;                 %   model
 dlambda            = 0.25;                 % separation of wavelength grid      Å
 k                  = 20;                      % rank of non-diagonal contribution
-max_noise_variance = 2^2;                     % maximum pixel noise allowed during model training
+max_noise_variance = 4^2;                     % maximum pixel noise allowed during model training
 
 % optimization parameters
 minFunc_options =               ...           % optimization options for model fitting
