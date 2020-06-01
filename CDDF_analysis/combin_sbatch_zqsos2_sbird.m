dacfb6b00bbfedc26210ace7ae11d922d815cf98
jibanCat <r04244007@ntu.edu.tw>
2020-05-31 17:13:02 -0700

    add a hard-coded script to combine all pieces of .mat files

--- CDDF_analysis/combin_sbatch_zqsos2_sbird.m
+++ CDDF_analysis/combin_sbatch_zqsos2_sbird.m
@@ -1,181 +1,208 @@
 % combine_sbatch.m: combine sbatch pieces using hardcoded MATLAB for loop
 
+% be sure you get the right normalization range!
+set_parameters;
+
 % hard code these init vars since this is a backup script to when sbatch_reunion
 % is not working anyway
-release           = 'dr14q';
-test_set_name     = 'dr14q';
+release           = 'dr12q';
+test_set_name     = 'dr12q';
 
-max_dlas          = 4;
-num_quasars       = 188355;
-num_quasars_split = 1000;
-num_files         = 188;
-num_final_chunk   = 355;
+% max_dlas          = 4;
+num_quasars_offset =  80000;
+num_quasars        =  90000;
+num_quasars_split  =     50;
+num_files          = floor((num_quasars - num_quasars_offset) / num_quasars_split);
+num_final_chunk    =      0;
 
+% the size of sub and sup depends on this
+DLA_cut = 20.3;
 
 variables_to_load = {'training_release', 'training_set_name', ...
-                     'dla_catalog_name', 'prior_ind', 'release', ...
-                     'test_set_name', 'prior_z_qso_increase', 'k', ...
-                     'normalization_min_lambda', 'normalization_max_lambda', ...
-                     'min_z_cut', 'max_z_cut', 'num_dla_samples', ...
-                     'num_lines', 'test_ind'};
+    'dla_catalog_name', 'release', ...
+    'test_set_name', 'test_ind', 'prior_z_qso_increase', ...
+    'max_z_cut', 'num_lines', 'all_thing_ids', ...
+    'sample_log_posteriors_dla_sub'}; % load this to verify the size
 
-load(sprintf('%s/processed_qsos_multi_lls_occam_lyseries_variance_kim_lyb_a03_dr14q_0-1000',             ...
-     processed_directory(release)), ...
+% to fit MATLAB's convention, you start with +1
+quasar_start_ind = num_quasars_offset + 1;
+quasar_end_ind   = num_quasars_offset + num_quasars_split + 1;
+
+load(sprintf('%s/%s/processed_qsos_zqsos_sbird_%s-%s_%d-%d_norm_%d-%d',             ...
+     processed_directory(release), ...
+     test_set_name, optTag, ...
+     quasar_start_ind, quasar_end_ind, ...
+     normalization_min_lambda, normalization_max_lambda), ...
      variables_to_load{:});
+
+% sup and sub have different size than num_dla_samples due to they are acquire by
+% logNHI >= 20.3 and logNHI < 20.3; so load these two variables first to verify the size
+% load DLA samples from training release
+dla_samples_to_load = {'log_nhi_samples'};
+load(sprintf('%s/dla_samples', processed_directory(training_release)), ...
+    dla_samples_to_load{:});
+
+% check the size is correct
+sub20pt3_ind  = (log_nhi_samples < DLA_cut);
+sub20pt3_size = sum(sub20pt3_ind);
+[~, sub_size] = size(sample_log_posteriors_dla_sub);
+assert( sub_size == sub20pt3_size )
+
+% assign the sub and sup size, based on DLA_cut
+sub_size = sub20pt3_size;
+sup_size = num_dla_samples - sub20pt3_size;
 
 % init large arrays to append
 % variables_to_append
-min_z_dlas                 = nan(num_quasars, 1);
-max_z_dlas                 = nan(num_quasars, 1);
-log_priors_no_dla          = nan(num_quasars, 1);
-log_priors_dla             = nan(num_quasars, max_dlas);
-log_likelihoods_no_dla     = nan(num_quasars, 1);
-sample_log_likelihoods_dla = nan(num_quasars, num_dla_samples, max_dlas);
-base_sample_inds           = zeros(num_quasars, num_dla_samples, max_dlas - 1, 'uint32');
-log_likelihoods_dla        = nan(num_quasars, max_dlas);
-log_posteriors_no_dla      = nan(num_quasars, 1);
-log_posteriors_dla         = nan(num_quasars, max_dlas);
+min_z_dlas                    = nan(num_quasars, num_dla_samples);
+max_z_dlas                    = nan(num_quasars, num_dla_samples);
+sample_log_posteriors_no_dla  = nan(num_quasars, num_dla_samples);
+sample_log_posteriors_dla     = nan(num_quasars, num_dla_samples);
+sample_log_posteriors_dla_sub = nan(num_quasars, sub_size);
+sample_log_posteriors_dla_sup = nan(num_quasars, sup_size);
+log_posteriors_no_dla         = nan(num_quasars, 1);
+log_posteriors_dla            = nan(num_quasars, 1);
+log_posteriors_dla_sub        = nan(num_quasars, 1);
+log_posteriors_dla_sup        = nan(num_quasars, 1);
+z_true                        = nan(num_quasars, 1);
+dla_true                      = nan(num_quasars, 1);
+z_map                         = nan(num_quasars, 1);
+z_dla_map                     = nan(num_quasars, 1);
+n_hi_map                      = nan(num_quasars, 1);
+log_nhi_map                   = nan(num_quasars, 1);
+signal_to_noise               = nan(num_quasars, 1);
 
-% initialize lls results
-log_likelihoods_lls        = nan(num_quasars, 1);
-log_posteriors_lls         = nan(num_quasars, 1);
-log_priors_lls             = nan(num_quasars, 1);
-sample_log_likelihoods_lls = nan(num_quasars, num_dla_samples);
-
-% save maps: add the initilizations of MAP values
-% N * (1~k models) * (1~k MAP dlas)
-MAP_z_dlas   = nan(num_quasars, max_dlas, max_dlas);
-MAP_log_nhis = nan(num_quasars, max_dlas, max_dlas);
-
-all_exceptions = nan(num_quasars, 1);
 
 % posteriors results which are not in init of process script
-model_posteriors = nan(num_quasars, max_dlas + 2);
+model_posteriors = nan(num_quasars, 2);
 p_no_dlas        = nan(num_quasars, 1);
 p_dlas           = nan(num_quasars, 1);
-p_lls            = nan(num_quasars, 1);
 
 % variables_to_append : from each small file
-variables_to_append = {'min_z_dlas', 'max_z_dlas', ...
-    'sample_log_likelihoods_dla', 'base_sample_inds', ...
-    'log_priors_no_dla', 'log_priors_dla', 'log_priors_lls', ...
-    'log_likelihoods_no_dla', 'MAP_z_dlas', 'MAP_log_nhis', ...
-    'log_likelihoods_dla', 'log_likelihoods_lls', ...
-    'log_posteriors_no_dla', 'log_posteriors_dla', 'log_posteriors_lls', ...
-    'model_posteriors', 'p_no_dlas', 'p_dlas', 'p_lls', ...
-    'all_exceptions', 'sample_log_likelihoods_lls'};
-
+variables_to_load = {'min_z_dlas', 'max_z_dlas', ... % you need to save DLA search length to compute CDDF
+     'sample_log_posteriors_no_dla', 'sample_log_posteriors_dla', ...
+     'sample_log_posteriors_dla_sub', 'sample_log_posteriors_dla_sup', ...
+     'log_posteriors_no_dla', 'log_posteriors_dla', ...
+     'log_posteriors_dla_sub', 'log_posteriors_dla_sup', ...
+     'model_posteriors', 'p_no_dlas', ...
+     'p_dlas', 'z_map', 'z_true', 'dla_true', 'z_dla_map', 'n_hi_map', 'log_nhi_map',...
+     'signal_to_noise'};
+     %'all_posdeferrors', 'all_exceptions', 'qso_ind', % append these variables to complete the catalog
 
 for i = 0:(num_files - 1)
     % only load variables to append to save mamory usage
-    processed = load(sprintf('%s/processed_qsos_multi_lls_occam_lyseries_variance_kim_lyb_a03_dr14q_%d-%d', ...
-         processed_directory(release), ...
-         i * num_quasars_split, (i + 1) * num_quasars_split ), ...
-         variables_to_append{:});
+    % to fit MATLAB's convention, you start with +1
+    quasar_start_ind = num_quasars_offset + 1 + num_quasars_split * i;
+    quasar_end_ind   = quasar_start_ind + num_quasars_split;
+
+    processed = load(sprintf('%s/%s/processed_qsos_zqsos_sbird_%s-%s_%d-%d_norm_%d-%d',             ...
+        processed_directory(release), ...
+        test_set_name, optTag, ...
+        quasar_start_ind, quasar_end_ind, ...
+        normalization_min_lambda, normalization_max_lambda), ...
+        variables_to_load{:});
     
-    qso_start = i * num_quasars_split + 1;
-    qso_end   = (i + 1) * num_quasars_split;
+    % annoying MATLAB convention: they include both the initial index and the end index when slicing
+    qso_start =  i      * num_quasars_split + 1;   % need + 1
+    qso_end   = (i + 1) * num_quasars_split;       % no need + 1
     
     % hardcoded to append everything
     % variables_to_append
-    min_z_dlas(qso_start:qso_end, 1)             = processed.min_z_dlas(:, 1);
-    max_z_dlas(qso_start:qso_end, 1)             = processed.max_z_dlas(:, 1);
-    log_priors_no_dla(qso_start:qso_end, 1)      = processed.log_priors_no_dla(:, 1);
-    log_priors_dla(qso_start:qso_end, :)         = processed.log_priors_dla(:, :);
-    log_likelihoods_no_dla(qso_start:qso_end, 1) = processed.log_likelihoods_no_dla(:, 1);
-    sample_log_likelihoods_dla(qso_start:qso_end, :, :) = processed.sample_log_likelihoods_dla(:, :, :);
-    base_sample_inds(qso_start:qso_end, :, :)    = processed.base_sample_inds(:, :, :);
-    log_likelihoods_dla(qso_start:qso_end, :)    = processed.log_likelihoods_dla(:, :);
+    min_z_dlas(qso_start:qso_end, :)             = processed.min_z_dlas(:, :);
+    max_z_dlas(qso_start:qso_end, :)             = processed.max_z_dlas(:, :);
+
+    sample_log_posteriors_no_dla(qso_start:qso_end, :)  = processed.sample_log_posteriors_no_dla(:, :);
+    sample_log_posteriors_dla(qso_start:qso_end, :)     = processed.sample_log_posteriors_dla(:, :);
+    sample_log_posteriors_dla_sub(qso_start:qso_end, :) = processed.sample_log_posteriors_dla_sub(:, :);
+    sample_log_posteriors_dla_sup(qso_start:qso_end, :) = processed.sample_log_posteriors_dla_sup(:, :);
+    
     log_posteriors_no_dla(qso_start:qso_end, 1)  = processed.log_posteriors_no_dla(:, 1);
-    log_posteriors_dla(qso_start:qso_end, :)     = processed.log_posteriors_dla(:, :);
+    log_posteriors_dla(qso_start:qso_end, 1)     = processed.log_posteriors_dla(:, 1);
+    log_posteriors_dla_sub(qso_start:qso_end, 1) = processed.log_posteriors_dla_sub(:, 1);
+    log_posteriors_dla_sup(qso_start:qso_end, 1) = processed.log_posteriors_dla_sup(:, 1);
 
-    % initialize lls results
-    log_likelihoods_lls(qso_start:qso_end, 1) = processed.log_likelihoods_lls(:, 1);
-    log_posteriors_lls(qso_start:qso_end, 1)  = processed.log_posteriors_lls(:, 1);
-    log_priors_lls(qso_start:qso_end, 1)      = processed.log_priors_lls(:, 1);
-    sample_log_likelihoods_lls(qso_start:qso_end, :) = processed.sample_log_likelihoods_lls(:, :);
-
-    % save maps: add the initilizations of MAP values
-    % N * (1~k models) * (1~k MAP dlas)
-    MAP_z_dlas(qso_start:qso_end, :, :)   = processed.MAP_z_dlas(:, :, :);
-    MAP_log_nhis(qso_start:qso_end, :, :) = processed.MAP_log_nhis(:, :, :);
-
-    all_exceptions(qso_start:qso_end, 1)  = processed.all_exceptions(:, 1);
+    z_true(qso_start:qso_end, 1)          = processed.z_true(:, 1);
+    dla_true(qso_start:qso_end, 1)        = processed.dla_true(:, 1);
+    z_map(qso_start:qso_end, 1)           = processed.z_map(:, 1);
+    z_dla_map(qso_start:qso_end, 1)       = processed.z_dla_map(:, 1);
+    n_hi_map(qso_start:qso_end, 1)        = processed.n_hi_map(:, 1);
+    log_nhi_map(qso_start:qso_end, 1)     = processed.log_nhi_map(:, 1);
+    signal_to_noise(qso_start:qso_end, 1) = processed.signal_to_noise(:, 1);    
 
     % posteriors results which are not in init of process script
     model_posteriors(qso_start:qso_end, :) = processed.model_posteriors(:, :);
-    p_no_dlas(qso_start:qso_end, 1)        = processed.model_posteriors(:, 1);
+    p_no_dlas(qso_start:qso_end, 1)        = processed.p_no_dlas(:, 1);
     p_dlas(qso_start:qso_end, 1)           = processed.p_dlas(:, 1);
-    p_lls(qso_start:qso_end, 1)            = processed.p_lls(:, 1);
 
     clear processed    
 end
 
-% ad-hocly add the final piece of chunck
-processed = load(sprintf('%s/processed_qsos_multi_lls_occam_lyseries_variance_kim_lyb_a03_dr14q_%d-%d', ...
-    processed_directory(release), ...
-    num_files * num_quasars_split, ...
-    num_files * num_quasars_split + num_final_chunk), ...
-    variables_to_append{:});
+if (num_final_chunk ~= 0)
+    % ad-hocly add the final piece of chunck
+    quasar_start_ind = num_quasars_offset + num_files * num_quasars_split + 1;
+    quasar_end_ind   = quasar_start_ind + num_final_chunk;
 
-qso_start = num_files * num_quasars_split + 1;
-qso_end   = num_files * num_quasars_split + num_final_chunk;
+    processed = load(sprintf('%s/%s/processed_qsos_zqsos_sbird_%s-%s_%d-%d_norm_%d-%d',             ...
+        processed_directory(release), ...
+        test_set_name, optTag, ...
+        quasar_start_ind, quasar_end_ind, ...
+        normalization_min_lambda, normalization_max_lambda), ...
+        variables_to_load{:});
 
-% hardcoded to append everything
-% variables_to_append
-min_z_dlas(qso_start:qso_end, 1)             = processed.min_z_dlas(:, 1);
-max_z_dlas(qso_start:qso_end, 1)             = processed.max_z_dlas(:, 1);
-log_priors_no_dla(qso_start:qso_end, 1)      = processed.log_priors_no_dla(:, 1);
-log_priors_dla(qso_start:qso_end, :)         = processed.log_priors_dla(:, :);
-log_likelihoods_no_dla(qso_start:qso_end, 1) = processed.log_likelihoods_no_dla(:, 1);
-sample_log_likelihoods_dla(qso_start:qso_end, :, :) = processed.sample_log_likelihoods_dla(:, :, :);
-base_sample_inds(qso_start:qso_end, :, :)    = processed.base_sample_inds(:, :, :);
-log_likelihoods_dla(qso_start:qso_end, :)    = processed.log_likelihoods_dla(:, :);
-log_posteriors_no_dla(qso_start:qso_end, 1)  = processed.log_posteriors_no_dla(:, 1);
-log_posteriors_dla(qso_start:qso_end, :)     = processed.log_posteriors_dla(:, :);
+    qso_start = num_quasars_offset + num_files * num_quasars_split + 1;               % need + 1
+    qso_end   = num_quasars_offset + num_files * num_quasars_split + num_final_chunk; % no need + 1
 
-% initialize lls results
-log_likelihoods_lls(qso_start:qso_end, 1) = processed.log_likelihoods_lls(:, 1);
-log_posteriors_lls(qso_start:qso_end, 1)  = processed.log_posteriors_lls(:, 1);
-log_priors_lls(qso_start:qso_end, 1)      = processed.log_priors_lls(:, 1);
-sample_log_likelihoods_lls(qso_start:qso_end, :) = processed.sample_log_likelihoods_lls(:, :);
+    % hardcoded to append everything
+    % variables_to_append
+    min_z_dlas(qso_start:qso_end, :)             = processed.min_z_dlas(:, :);
+    max_z_dlas(qso_start:qso_end, :)             = processed.max_z_dlas(:, :);
 
-% save maps: add the initilizations of MAP values
-% N * (1~k models) * (1~k MAP dlas)
-MAP_z_dlas(qso_start:qso_end, :, :)   = processed.MAP_z_dlas(:, :, :);
-MAP_log_nhis(qso_start:qso_end, :, :) = processed.MAP_log_nhis(:, :, :);
+    sample_log_posteriors_no_dla(qso_start:qso_end, :)  = processed.sample_log_posteriors_no_dla(:, :);
+    sample_log_posteriors_dla(qso_start:qso_end, :)     = processed.sample_log_posteriors_dla(:, :);
+    sample_log_posteriors_dla_sub(qso_start:qso_end, :) = processed.sample_log_posteriors_dla_sub(:, :);
+    sample_log_posteriors_dla_sup(qso_start:qso_end, :) = processed.sample_log_posteriors_dla_sup(:, :);
+    
+    log_posteriors_no_dla(qso_start:qso_end, 1)  = processed.log_posteriors_no_dla(:, 1);
+    log_posteriors_dla(qso_start:qso_end, 1)     = processed.log_posteriors_dla(:, 1);
+    log_posteriors_dla_sub(qso_start:qso_end, 1) = processed.log_posteriors_dla_sub(:, 1);
+    log_posteriors_dla_sup(qso_start:qso_end, 1) = processed.log_posteriors_dla_sup(:, 1);
 
-all_exceptions(qso_start:qso_end, 1)  = processed.all_exceptions(:, 1);
+    z_true(qso_start:qso_end, 1)          = processed.z_true(:, 1);
+    dla_true(qso_start:qso_end, 1)        = processed.dla_true(:, 1);
+    z_map(qso_start:qso_end, 1)           = processed.z_map(:, 1);
+    z_dla_map(qso_start:qso_end, 1)       = processed.z_dla_map(:, 1);
+    n_hi_map(qso_start:qso_end, 1)        = processed.n_hi_map(:, 1);
+    log_nhi_map(qso_start:qso_end, 1)     = processed.log_nhi_map(:, 1);
+    signal_to_noise(qso_start:qso_end, 1) = processed.signal_to_noise(:, 1);    
 
-% posteriors results which are not in init of process script
-model_posteriors(qso_start:qso_end, :) = processed.model_posteriors(:, :);
-p_no_dlas(qso_start:qso_end, 1)        = processed.model_posteriors(:, 1);
-p_dlas(qso_start:qso_end, 1)           = processed.p_dlas(:, 1);
-p_lls(qso_start:qso_end, 1)            = processed.p_lls(:, 1);
+    % posteriors results which are not in init of process script
+    model_posteriors(qso_start:qso_end, :) = processed.model_posteriors(:, :);
+    p_no_dlas(qso_start:qso_end, 1)        = processed.p_no_dlas(:, 1);
+    p_dlas(qso_start:qso_end, 1)           = processed.p_dlas(:, 1);
 
-clear processed
+    clear processed    
+end
 
 fprintf_debug('Number of isnan : %d; number of all_exceptions : %d', ...
-    sum(isnan(log_likelihoods_no_dla)), sum(~isnan(all_exceptions)));
+    sum(isnan(log_posteriors_no_dla)), sum(~isnan(log_posteriors_dla_sup)));
 
 % save results
 % exclude base_sample_inds and sample_log_likelihoods_dla
 variables_to_save = {'training_release', 'training_set_name', ...
-                     'dla_catalog_name', 'prior_ind', 'release', ...
-                     'test_set_name', 'prior_z_qso_increase', 'k', ...
-                     'normalization_min_lambda', 'normalization_max_lambda', ...
-                     'min_z_cut', 'max_z_cut', 'num_dla_samples', ...
-                     'num_lines', 'min_z_dlas', 'max_z_dlas', ...
-                     'sample_log_likelihoods_dla', 'base_sample_inds', ...
-                     'log_priors_no_dla', 'log_priors_dla', 'log_priors_lls', ...
-                     'log_likelihoods_no_dla', 'MAP_z_dlas', 'MAP_log_nhis', ...
-                     'log_likelihoods_dla', 'log_likelihoods_lls', ...
-                     'log_posteriors_no_dla', 'log_posteriors_dla', 'log_posteriors_lls', ...
-                     'model_posteriors', 'p_no_dlas', 'p_dlas', 'p_lls', ...
-                     'all_exceptions', 'sample_log_likelihoods_lls', 'test_ind'};
+                     'dla_catalog_name', 'release', ...
+                     'test_set_name', 'test_ind', 'prior_z_qso_increase', ...
+                     'max_z_cut', 'num_lines', 'min_z_dlas', 'max_z_dlas', ... % you need to save DLA search length to compute CDDF
+                     'sample_log_posteriors_no_dla', 'sample_log_posteriors_dla', ...
+                     'sample_log_posteriors_dla_sub', 'sample_log_posteriors_dla_sup', ...
+                     'log_posteriors_no_dla', 'log_posteriors_dla', ...
+                     'log_posteriors_dla_sub', 'log_posteriors_dla_sup', ...
+                     'model_posteriors', 'p_no_dlas', ...
+                     'p_dlas', 'z_map', 'z_true', 'dla_true', 'z_dla_map', 'n_hi_map', 'log_nhi_map',...
+                     'signal_to_noise', 'all_thing_ids'};
 
-filename = sprintf('%s/processed_qsos_multi_lls_occam_lyseries_variance_kim_lyb_a03_%s', ...
+filename = sprintf('%s/processed_qsos_zqsos_sbird_%s-%s_norm_%d-%d', ...
                     processed_directory(release), ...
-                    test_set_name);
+                    test_set_name, optTag, ...
+                    normalization_min_lambda, normalization_max_lambda);
 
 save(filename, variables_to_save{:}, '-v7.3');
